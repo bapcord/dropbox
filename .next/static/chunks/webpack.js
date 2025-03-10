487ad2b79d0ab61ef2479e2271092ea94ba92837
Your Name <your.email@example.com>
2025-03-10 16:35:36 -0400

    Fix MongoDB connection string encoding

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fe5f5961912b4fc7"; }
+/******/ 		__webpack_require__.h = function() { return "3311d1ace1bc6ba2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
